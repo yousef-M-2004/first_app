d252f1af8b7f38b8c628776192ce0f8f38e0a90a
notmozart0 <notmozart0@gmail.com>
2026-05-16 17:38:06 +0300

    Add files via upload

--- features/auth_and_users/auth_routes.js
+++ features/auth_and_users/auth_routes.js
@@ -1,8 +1,33 @@
+const express = require('express');
 const router = require("express").Router();
-//const auth = require("./auth_and_users/auth_middleware.js");
+const bcrypt = require ('bcrypt');
+router.use(express.json());
+const {sign_up , users,login} = require("./auth_controller.js");
 
+//const auth = require("/auth_middleware.js");
+//home page
 router.get("/",(req,res)=>{
+    res.send("home page")
+})
+//const users = []
+//login route
+router.get("/login",(req,res)=>{
+    res.send("login page ")
+})
+router.post("/login",login ,async(req,res)=>{
     res.send("hello")
 })
+// sign up route
+router.get("/sign_up ",(req,res)=>{
+    res.send("sign up page")
+})
 
-module.exports = router;
+
+router.post("/sign_up", sign_up ,async (req,res)=>{
+  
+    console.log (users);
+})
+
+module.exports = {
+    router,
+};
