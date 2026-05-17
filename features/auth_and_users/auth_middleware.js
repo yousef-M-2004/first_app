0d212dea952494da3e0fc3065895c25e15a90342
notmozart0 <notmozart0@gmail.com>
2026-05-17 21:24:09 +0300

    Add files via upload

--- features/auth_and_users/auth_middleware.js
+++ features/auth_and_users/auth_middleware.js
@@ -1,1 +1,21 @@
+const env = require("dotenv").configure ; 
+const jwt = require("jsonwebtoken")
 
+
+const jwt_auth =(req,res,next) =>{
+
+    const header = req.headers.authorization ;
+    if (!header ){
+        return res.status(402).send("No token");
+    }
+    const token =header.split(" ")[1];
+    const user =
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN
+    );
+    req.user =user ; 
+    next ();
+
+}
+module.exports = {jwt_auth}
